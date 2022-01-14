7dac9a13562c679c0421f3ad38d258cc5a033f47
ada <adacu90@gmail.com>
2022-01-14 17:41:31 -0500

    theme install for init

---
+++ wp-content/themes/bootscore-main/template-home.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Template Name: Home
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Bootscore
+ */
+
+get_header();
+?>
+<div id="content" class="site-content container py-5 mt-5">
+ culo
+</div><!-- #content -->
+<?php
+get_footer();

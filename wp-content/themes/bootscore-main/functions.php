2bb8a53d73cb9903ffc0bfae83e4bfdf0b83a666
Osdeibi <osdeibi@gmail.com>
2022-01-21 21:42:45 -0500

    add custom CSS for theme

--- wp-content/themes/bootscore-main/functions.php
+++ wp-content/themes/bootscore-main/functions.php
@@ -251,7 +251,8 @@ function bootscore_scripts() {
   } else {
     $modificated_bootscoreCss = 1;
   }
-  $modificated_fontawesomeCss = date('YmdHi', filemtime(get_template_directory() . '/css/lib/fontawesome.min.css'));
+	$modificated_fontawesomeCss = date('YmdHi', filemtime(get_template_directory() . '/css/lib/fontawesome.min.css'));
+	$modificated_themeCss = date('YmdHi', filemtime(get_template_directory() . '/css/theme.css'));
   $modificated_bootstrapJs = date('YmdHi', filemtime(get_template_directory() . '/js/lib/bootstrap.bundle.min.js'));
   $modificated_themeJs = date('YmdHi', filemtime(get_template_directory() . '/js/theme.js'));
 
@@ -263,8 +264,11 @@ function bootscore_scripts() {
   bootscore_compile_scss();
   wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/lib/bootstrap.min.css', array(), $modificated_bootscoreCss);
 
-  // Fontawesome
-  wp_enqueue_style('fontawesome', get_template_directory_uri() . '/css/lib/fontawesome.min.css', array(), $modificated_fontawesomeCss);
+  // Custom CSS for theme
+  wp_enqueue_style('themeCSS', get_template_directory_uri() . '/css/theme.css', array(), $modificated_themeCss);
+
+	// Fontawesome
+	wp_enqueue_style('fontawesome', get_template_directory_uri() . '/css/lib/fontawesome.min.css', array(), $modificated_fontawesomeCss);
 
   // Bootstrap JS
   wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/lib/bootstrap.bundle.min.js', array(), $modificated_bootstrapJs, true);
